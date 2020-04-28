9b1343a401e58db471a18e50840d46e494995173
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-04-28 01:13:37 -0600

    worked on get and post logic

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+    name: "Ernest",
+    photo: "#",
+    scores: [
+        8,
+        3,
+        5,
+        1,
+        1,
+        9,
+        7,
+        2,
+        4,
+        6
+    ]
+}
